aa12553d2d98ee56b22337246f44b62ce7bb8c5c
shazy89 <erdo.shazy123@gmail.com>
2021-02-28 11:37:07 -0500

    initial setup server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-//Main starting point
+//Main starting point of the application
+const express = require("express");
+
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+//const router = require("./router");
+const mongoose = require("mongoose");
+//const cors = require("cors");
+const app = express();
+// App setup
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("server listening on:", port);
