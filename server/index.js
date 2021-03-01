1ff146024e6545d7cd93831fc4d06f16e0737557
shazy89 <erdo.shazy123@gmail.com>
2021-03-01 11:24:48 -0500

    connected to mongo db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 //Main starting point of the application
 const express = require("express");
+require("dotenv").config();
 
 const http = require("http");
 const bodyParser = require("body-parser");
@@ -9,10 +10,30 @@ const router = require("./router");
 const mongoose = require("mongoose");
 //const cors = require("cors");
 const app = express();
+
 // App setup
 app.use(morgan("combined"));
-app.use(bodyParser.json({ type: "*/*" }));
+app.use(bodyParser.json());
 router(app);
+
+const mongoUri = `${process.env.MONGO_URI}`;
+const connectDb = async () => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+connectDb();
+
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
